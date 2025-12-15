f2ebd20819baad4899a46fc3b0ec209d0b56db34
XxpolipxX <adam.gabrys@poczta.onet.pl>
2025-12-15 15:59:01 +0100

    rozwiazanie

---
+++ 2022-06-03/zgloszenie.php
@@ -0,0 +1,12 @@
+<?php
+    $polaczenie = mysqli_connect('localhost', 'root', '', 'wedkarstwo');
+
+    $lowisko = $_POST['lowisko'];
+    $data = $_POST['data'];
+    $sedzia = $_POST['sedzia'];
+
+    $zapytanie = 'INSERT INTO `zawody_wedkarskie`(`id`, `Karty_wedkarskie_id`, `Lowisko_id`, `data_zawodow`, `sedzia`) VALUES (NULL, 0, ' . $lowisko . ', "' . $data . '", "' . $sedzia . '");';
+    mysqli_query($polaczenie, $zapytanie);
+
+    mysqli_close($polaczenie);
+?>
